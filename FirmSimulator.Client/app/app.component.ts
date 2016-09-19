f20cc087899899a2557ddb2998f1324405c5d1ea
Aleks Angelov <aia131@aubg.edu>
2016-09-19 23:49:22 +0300

    Clean templates

---
+++ FirmSimulator.Client/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "my-app",
+    template: "<h1>Microeconomic Simulator of Firm Behavior under Monopolistic Competition</h1>"
+})
+export class AppComponent {
+}
