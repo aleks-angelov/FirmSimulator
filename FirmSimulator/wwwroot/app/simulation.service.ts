014a0288da047392f0364ec9f3db151ad613c2b7
aleks-angelov <aia131@aubg.edu>
2016-10-26 20:23:25 +0300

    Upgraded to Angular 2.1.1

--- FirmSimulator/wwwroot/app/simulation.service.ts
+++ FirmSimulator/wwwroot/app/simulation.service.ts
@@ -24,20 +24,6 @@ export class SimulationService {
         return this.costModel;
     }
 
-    testValues(Q: number) {
-        const price = this.revenueModel.calculatePrice(Q);
-        const tr = this.revenueModel.calculateTotalRevenue(Q);
-        const mr = this.revenueModel.calculateMarginalRevenue(Q);
-        const tc = this.costModel.calculateTotalCost(Q);
-        const ac = this.costModel.calculateAverageCost(Q);
-        const mc = this.costModel.calculateMarginalCost(Q);
-        const profit = tr - tc;
-
-        console.log(`Settings: ${this.settingsDescription}\n\nPrice: $${price.toFixed(2)}\nTotal Revenue: $${tr.toFixed(2)}\nMarginal Revenue: $${mr
-            .toFixed(2)}\n\nTotal Cost: $${tc.toFixed(2)}\nAverage Cost: $${ac.toFixed(2)}\nMarginal Cost: $${mc
-                .toFixed(2)}\n\nProfit: $${profit.toFixed(2)}`);
-    }
-
     isSimulationRunning(): boolean {
         return this.settingsDescription != null;
     }
@@ -46,7 +32,6 @@ export class SimulationService {
         this.settingsDescription = initialSettings.description;
         this.revenueModel = new Revenue(initialSettings.revenue_a, initialSettings.revenue_b);
         this.costModel = new Cost(initialSettings.cost_a, initialSettings.cost_b, initialSettings.cost_c);
-        this.testValues(12);
     }
 
     makeTurn() {
